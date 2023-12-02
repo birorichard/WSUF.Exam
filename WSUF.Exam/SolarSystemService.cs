99ac3c5db7b6f81e45e2663a4ae0a0bce75cc610
Richard <birorichard36@gmail.com>
2023-12-02 12:30:22 +0100

    kis help

--- WSUF.Exam/SolarSystemService.cs
+++ WSUF.Exam/SolarSystemService.cs
@@ -35,10 +35,51 @@ namespace WSUF.Exam
                 {
                     if (body.IsPlanet)
                     {
-                        Console.WriteLine($"{body.EnglishName}: {body.Gravity} N");
+                        // 1. kiiras
+                        if (body.EnglishName == "Moon")
+                        {
+                            // 2. kiiras
+                            Console.WriteLine($"{body.EnglishName}: {body.Gravity} N");
+
+                        }
                     }
                 }
             }
         }
+
+        public void WriteDetailedPlanetInfo()
+        {
+            // 1. Lekerjuk az egitesteket
+            List<SolarSystemBody> bodies = _solarSystemClient.GetAllBodies();
+
+            // 2. Null check
+            if (bodies != null)
+            {
+                // 3. foreach ciklus használata (bodies: osszes egitest, body: aktualisan vizsgalt egitest)
+                foreach (var body in bodies)
+                {
+                    if (body.IsPlanet)
+                    {
+                        // 1. kiiras body.Name body.EnglishName
+                        // hasonlit erre:
+                        Console.WriteLine($"{body.EnglishName}: {body.Gravity}");
+
+                        // meg egy if, amiben vizsgaljuk, hogy a body.Gravity > 5 ?
+                        if (?)
+                        {
+                            // 2. Kiiras - irjuk ki a body.Gravity-t
+                            Console.WriteLine();
+                        }
+                    }
+                }
+            }
+        }
+
+        // public metodus, nincs visszateresi erteke, a neve pedig: WriteDetailedPlanetInfo
+        // Az osszes egitest kozul, ami bolygo tipusu, azoknak ird ki a hagyomanyos, illetve angol nevet
+        // egymas melle szokozzel elvalasztva (hagyomanyos nev: Name, az angol nev: EnglishName).
+
+        // Egy uj sorban pedig legyen kiirva, hogy mekkora a gravitacioja, de csak akkor, ha a gravitacios
+        // nagyobb mint 5. (gravitacio: Gravity)
     }
 }
